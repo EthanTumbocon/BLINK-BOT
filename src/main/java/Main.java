1335bcc78bf972e4c80bd4aa751643e8c9802e62
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2021-01-29 20:22:42 -0600

    starting over because mistakes were made

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,11 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
 
-import javax.security.auth.login.LoginException;
 
 public class Main {
-//https://discord.com/oauth2/authorize?client_id=<client_id>&scope=bot;
     public static void main(String[] args) {
-        JDABuilder jdaBuilder = JDABuilder.createDefault("!token");
-        BlackPink blackPink = new BlackPink();
-        JDABuilder jdaBuilder1 = JDABuilder.addEventListeners(blackPink);
-        JDABuilder.setActivity(Activity.watching("Listening to kpop!"));
-        JDA jda = null;
-        try {
-            jda = jdaBuilder.build();
-        } catch (LoginException e) {
-            e.printStackTrace();
-        }
-        try {
-            jda.awaitReady();
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
+        JDA jda = (JDA) JDABuilder.createDefault("!token");
+
     }
 }
 
