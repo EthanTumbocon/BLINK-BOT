bd37d9c5a0147b98a3dec7c7683c28fb78cf799f
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2021-02-03 20:43:32 -0600

    commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 
 public class Main {
     public static void main(String[] args) {
-        JDA jda = (JDA) JDABuilder.createDefault("!token");
+        JDABuilder jda =  JDABuilder.createDefault("!token");
+        // need to build a pathway to get youtube integrated
+        //need to set commands such as "-p (SONG)", -stop etc
 
     }
 }
